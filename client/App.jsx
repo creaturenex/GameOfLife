9e615b2caa45645f973413396bc1ee6631483fcb
Oscar M Romero <creaturenex@gmail.com>
2023-03-07 20:28:22 -0500

    initial state

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import 'bulma/css/bulma.min.css';
+import React from 'react';
+import { render } from 'react-dom';
+import Navbar from './components/Navbar.jsx'
+
+render (
+  <Navbar />
+);
+
+export default App;
